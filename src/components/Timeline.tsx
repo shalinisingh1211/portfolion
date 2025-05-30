7dfac0807c43a692ad65f5578fec901723faa680
shalinisingh1211 <121722088+shalinisingh1211@users.noreply.github.com>
2025-05-30 12:40:05 +0530

    Update Timeline.tsx

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,7 +16,6 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
@@ -47,7 +46,6 @@ function Timeline() {
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2020"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
