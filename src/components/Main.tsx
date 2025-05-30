23755f3f833a34aa5a77ea14189f264a791f8e2c
shalinisingh1211 <121722088+shalinisingh1211@users.noreply.github.com>
2025-05-30 12:45:52 +0530

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,9 +9,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-         <img src="/Screenshot(70).png" alt="Profile" />
-
-
+        
         </div>
         <div className="content">
           <div className="social_icons">
