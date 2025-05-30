535511352153d0fd9c3c89c57266a85644e45127
shalinisingh1211 <121722088+shalinisingh1211@users.noreply.github.com>
2025-05-30 12:24:42 +0530

    Update Project.tsx

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,7 +9,6 @@ import mock07 from '../assets/images/mock07.png';
 import mock08 from '../assets/images/mock08.png';
 import mock09 from '../assets/images/mock09.png';
 import mock10 from '../assets/images/mock10.png';
-import project1 from '../assets/image/project1.jpg';
 
 import '../assets/styles/Project.scss';
 
@@ -19,15 +18,14 @@ function Project() {
         <h1>Personal Projects</h1>
         <div className="projects-grid">
             <div className="project">
-               <img src={project1} className="zoom" alt="thumbnail" width="100%"/>
                 <h2>ImageGen </h2>
                 <h3>Tech Stack: Node.js, Express.js, MongoDB, Mongoose, Razorpay SDK, React.js, React Router,React Toastify</h3>
                 <p>A full-stack text-to-image generator app is built using React, Node.js, Express, and MongoDB. Users can register, log in, and generate images based on prompts, with a credit system for usage. The appintegrates Razorpay for payments, allowing users to purchase credits.</p>
             </div>
             <div className="project">
-                <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><h2>High Speed Chase</h2></a>
-                <p>Designed, developed, and launched a 3D multiplayer racing game with C# and Unity. This is available on Itch.io for gamers worldwide to enjoy.</p>
+               <h2>RiskRadar</h2>
+                <h3>TechStack:Python, Streamlit, SQLAlchemy, SpaCy, HuggingFace Transformers, LangChain</h3>
+                <p>Built an AI-assisted project risk management system to analyze and report risks across multipleprojects. Used SpaCy for natural language query parsing and Hugging Face models for generating riskmitigation suggestions. Implemented a document upload pipeline with versioned changelog storage.Integrated a Streamlit-based chatbot to answer project-specific risk queries using stored metrics andLLM-based fallback.</p>
             </div>
             <div className="project">
                 <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><img src={mock08} className="zoom" alt="thumbnail" width="100%"/></a>
