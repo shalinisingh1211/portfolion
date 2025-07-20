7ba6204fce76618cbbdfdd1a3e52e0822373ef59
shalinisingh1211 <121722088+shalinisingh1211@users.noreply.github.com>
2025-07-20 21:50:35 +0530

    Update Project.tsx

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -10,6 +10,7 @@ import mock08 from '../assets/images/mock08.png';
 import mock09 from '../assets/images/mock09.png';
 import mock10 from '../assets/images/mock10.png';
 import Yogap from '../assets/images/Yogap.png';
+import Risk from '../assets/images/Risk.png';
 
 import '../assets/styles/Project.scss';
 
@@ -28,7 +29,7 @@ can register, log in, and generate images based on prompts, with a credit system
 integrates Razorpay for payments, allowing users to purchase credits.</p>
             </div>
             <div className="project">
-                <h2><u>YogaPosePerfection</u</h2>
+                <h2><u>YogaPosePerfection</u></h2>
                 <h3>Tech Stack:HTML,CSS,Javascript,Deep Learning</h3>
                 <a href="https://shalinisingh1211.github.io/yoga-tracker-website/" target="_blank" rel="noreferrer"><img src={Yogap} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://drive.google.com/drive/folders/1_h0eq9ChGpzyWQvIKVP_5M18EHQiwyLl?usp=drive_link" target="_blank" rel="noreferrer"><h2><b>Project Code(Drive Link)</b></h2></a>
@@ -37,46 +38,15 @@ impressive accuracy in pose detection through the integration of TensorFlow and 
 real-time user feedback.</p>
             </div>
             <div className="project">
-                <h2><u>RiskRadar</u</h2>
+                <h2><u>RiskRadar</u></h2>
                 <h3>Tech Stack:Python, Streamlit, SQLAlchemy, SpaCy, HuggingFace Transformers, LangChain</h3>
-                <a href="https://drive.google.com/file/d/1GGyqbEwVa_p_07pATQXB2Jd5PyvbuD7v/view?usp=drive_link" rel="noreferrer"><img src={Risk} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><h2><b>Project Code(Drive Link)</b></h2></h2></a>
-                <p>Developed and released a 2D shooting game with C# and Unity. This project is hosted on the Itch.io public marketplace.</p>
-            </div>
-            <div className="project">
-                <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><img src={mock07} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><h2>Datum: Integrated Learning Platform</h2></a>
-                <p>This is an online educational platform that provides high-quality, data science-focused learning resources in the Japanese language. I created the entire platform from scratch using Ruby on Rails.</p>
-            </div>
-            <div className="project">
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><h2>WeManage: Real Estate Asset Management</h2></a>
-                <p>This mobile application allows realtors in Japan to securely manage their property information and view future income predictions. This app is built with Ruby on Rails and JavaScript.</p>
-            </div>
-            <div className="project">
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><img src={mock05} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><h2>COVID-19 Case Management</h2></a>
-                <p>Built official charts for COVID/vaccination tracking for an educational institution using JavaScript and the Google Sheets API v4. The dashboard served the university's leadership in their decision-making processes.</p>
-            </div>
-            <div className="project">
-                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><img src={mock04} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><h2>Multiple Regression Property Analysis</h2></a>
-                <p>Analyzed the real estate market in Japan and predicted property prices by implementing statistical methods such as OLS and multi-regression analysis. This project leveraged Python and various libraries such as Pandas, NumPy, Matplotlib, and Scikit-Learn.</p>
-            </div>
-            <div className="project">
-                <a href="https://holokai.byuh.edu/programs-of-study" target="_blank" rel="noreferrer"><img src={mock03} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://holokai.byuh.edu/programs-of-study" target="_blank" rel="noreferrer"><h2>Programs of Study</h2></a>
-                <p>Designed and developed a custom component for a CMS-based platform (e.g., 'Brightspot') using Java, Handlebars, and LESS. University students can find their majors of interest through this module.</p>
-            </div>
-            <div className="project">
-                <a href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix" target="_blank" rel="noreferrer"><img src={mock02} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix" target="_blank" rel="noreferrer"><h2>Transfer Evaluation Matrix</h2></a>
-                <p>Created an interactive CSV table generator with Java, Handlebars, and LESS. This project helps transfer students to quickly identify eligible credits.</p>
-            </div>
-            <div className="project">
-                <a href="https://github.com/yujisatojr/submeowrine" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/yujisatojr/submeowrine" target="_blank" rel="noreferrer"><h2>Submeowrine</h2></a>
-                <p>Developed and released an Android mobile application using Java and Android Studio that runs a 2D shooting game.</p>
+                <a href="https://drive.google.com/file/d/1GGyqbEwVa_p_07pATQXB2Jd5PyvbuD7v/view?usp=drive_link" target="_blank" rel="noreferrer"><img src={Risk} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://drive.google.com/drive/folders/1gaK7oOEB-p3IUi1luWeA8QQa-UncH_YS?usp=drive_link" target="_blank" rel="noreferrer"><h2><b>Project Code(Drive Link)</b></h2></a>
+                <p>Built an AI-assisted project risk management system to analyze and report risks across multiple
+projects. Used SpaCy for natural language query parsing and Hugging Face models for generating risk
+mitigation suggestions. Implemented a document upload pipeline with versioned changelog storage.
+Integrated a Streamlit-based chatbot to answer project-specific risk queries using stored metrics and
+LLM-based fallback.</p>
             </div>
         </div>
     </div>
